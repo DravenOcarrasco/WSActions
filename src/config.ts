4c0af7ee32bc955d855f0e3aa478508a9dae696d
MarcosBrendonDePaula <166177842+DravenOcarrasco@users.noreply.github.com>
2024-07-02 00:35:06 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import os from 'os';
 
 // Define a interface para a configuração
 interface Config {
@@ -8,11 +9,24 @@ interface Config {
     defaultPageUrl: string;
 }
 
+// Obtém o diretório do usuário atual
+const userHomeDir = os.homedir();
+
+// Configuração padrão
+const defaultConfig: Config = {
+    chromeProfilePath: path.join(userHomeDir, 'AppData', 'Local', 'Google', 'Chrome', 'User Data'),
+    scriptsPath: "./scripts/injector.js",
+    defaultPageUrl: "https://www.google.com"
+};
+
 // Função para carregar a configuração do arquivo config.json
 export const loadConfig = (): Config => {
     const configPath = path.join(process.cwd(), 'config.json');
+
     if (!fs.existsSync(configPath)) {
-        throw new Error(`Config file not found at ${configPath}`);
+        // Se o arquivo não existir, cria com a configuração padrão
+        fs.writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2));
+        console.log(`Config file created at ${configPath} with default settings.`);
     }
 
     const configFile = fs.readFileSync(configPath, 'utf-8');
